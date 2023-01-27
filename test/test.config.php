cba89cc7fd449bcc787a7411a4938ce0eee46c7f
ddave <icehrm@web-stalk.com>
2023-01-27 14:03:03 +0100

    Add composer file

--- test/test.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('TEST_BASE_PATH', dirname(__FILE__).'/');
-
-define('MYSQL_DB', 'testing');
-define('MYSQL_ROOT_USER', 'root');
-define('MYSQL_ROOT_PASS', 'dev');
--- test/test.includes.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!class_exists('TestTemplate')) {
-    include(dirname(__FILE__).'/TestTemplate.php');
-}
